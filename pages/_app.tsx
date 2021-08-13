9d24a6d3e46f0e950e1ca3f43e11a84e9675fcfd
Alwin van Driel <avandriel1998@gmail.com>
2021-08-13 18:11:34 +0200

    Add head component to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
+// Components
+import { Navbar } from '@organisms/navbar'
+import Head from 'next/head'
 // Styles
 import '@styles/globals.scss'
 import layoutStyles from '@styles/core/layout.module.scss'
 
-// Components
-import { Navbar } from '@organisms/navbar'
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Nextjs boilerplate</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Nextjs boilerplate" key="title" />
+      </Head>
       <Navbar>
         <span>Navigation</span>
       </Navbar>
